334a406668cb351aab539d12967417621f5dfce5
HariTheDemon <68599364+gg-cmd@users.noreply.github.com>
2023-06-15 08:10:09 +0100

    Create Translate.py

---
+++ Translate.py
@@ -0,0 +1,135 @@
+from googletrans import Translator
+import streamlit as st
+translator = Translator()
+
+options = [
+    ("Afrikaans", "af"),
+    ("Albanian", "sq"),
+    ("Amharic", "am"),
+    ("Arabic", "ar"),
+    ("Armenian", "hy"),
+    ("Azerbaijani", "az"),
+    ("Basque", "eu"),
+    ("Belarusian", "be"),
+    ("Bengali", "bn"),
+    ("Bosnian", "bs"),
+    ("Bulgarian", "bg"),
+    ("Catalan", "ca"),
+    ("Cebuano", "ceb"),
+    ("Chinese (Simplified)", "zh-CN"),
+    ("Chinese (Traditional)", "zh-TW"),
+    ("Corsican", "co"),
+    ("Croatian", "hr"),
+    ("Czech", "cs"),
+    ("Danish", "da"),
+    ("Dutch", "nl"),
+    ("English", "en"),
+    ("Esperanto", "eo"),
+    ("Estonian", "et"),
+    ("Finnish", "fi"),
+    ("French", "fr"),
+    ("Frisian", "fy"),
+    ("Galician", "gl"),
+    ("Georgian", "ka"),
+    ("German", "de"),
+    ("Greek", "el"),
+    ("Gujarati", "gu"),
+    ("Haitian Creole", "ht"),
+    ("Hausa", "ha"),
+    ("Hawaiian", "haw"),
+    ("Hebrew", "he"),
+    ("Hindi", "hi"),
+    ("Hmong", "hmn"),
+    ("Hungarian", "hu"),
+    ("Icelandic", "is"),
+    ("Igbo", "ig"),
+    ("Indonesian", "id"),
+    ("Irish", "ga"),
+    ("Italian", "it"),
+    ("Japanese", "ja"),
+    ("Javanese", "jv"),
+    ("Kannada", "kn"),
+    ("Kazakh", "kk"),
+    ("Khmer", "km"),
+    ("Korean", "ko"),
+    ("Kurdish", "ku"),
+    ("Kyrgyz", "ky"),
+    ("Lao", "lo"),
+    ("Latin", "la"),
+    ("Latvian", "lv"),
+    ("Lithuanian", "lt"),
+    ("Luxembourgish", "lb"),
+    ("Macedonian", "mk"),
+    ("Malagasy", "mg"),
+    ("Malay", "ms"),
+    ("Malayalam", "ml"),
+    ("Maltese", "mt"),
+    ("Maori", "mi"),
+    ("Marathi", "mr"),
+    ("Mongolian", "mn"),
+    ("Myanmar (Burmese)", "my"),
+    ("Nepali", "ne"),
+    ("Norwegian", "no"),
+    ("Nyanja (Chichewa)", "ny"),
+    ("Odia (Oriya)", "or"),
+    ("Pashto", "ps"),
+    ("Persian", "fa"),
+    ("Polish", "pl"),
+    ("Portuguese", "pt"),
+    ("Punjabi", "pa"),
+    ("Romanian", "ro"),
+    ("Russian", "ru"),
+    ("Samoan", "sm"),
+    ("Scots Gaelic", "gd"),
+    ("Serbian", "sr"),
+    ("Sesotho", "st"),
+    ("Shona", "sn"),
+    ("Sindhi", "sd"),
+    ("Sinhala (Sinhalese)", "si"),
+    ("Slovak", "sk"),
+    ("Slovenian", "sl"),
+    ("Somali", "so"),
+    ("Spanish", "es"),
+    ("Sundanese", "su"),
+    ("Swahili", "sw"),
+    ("Swedish", "sv"),
+    ("Tagalog (Filipino)", "tl"),
+    ("Tajik", "tg"),
+    ("Tamil", "ta"),
+    ("Tatar", "tt"),
+    ("Telugu", "te"),
+    ("Thai", "th"),
+    ("Turkish", "tr"),
+    ("Turkmen", "tk"),
+    ("Ukrainian", "uk"),
+    ("Urdu", "ur"),
+    ("Uyghur", "ug"),
+    ("Uzbek", "uz"),
+    ("Vietnamese", "vi"),
+    ("Welsh", "cy"),
+    ("Xhosa", "xh"),
+    ("Yiddish", "yi"),
+    ("Yoruba", "yo"),
+    ("Zulu", "zu")
+]
+st.set_page_config(page_title='Translator', page_icon=':tada:', layout='wide')
+
+st.title('This is a translation app i made for TopCoder')
+
+st.write('---')
+
+cont = st.container((1,2))
+
+col1, col2 = cont.beta_columns(2)
+
+with col1:
+    Var1 = st.text_input('Write your sentence here:  ')
+
+    Var2 = st.selectbox('select a language', options )
+    st.write('Please write the two letters beside the language of your choice!')
+
+
+with col2:
+
+    translated = translator.translate(Var1, dest=Var2)
+    print(translated.text)
